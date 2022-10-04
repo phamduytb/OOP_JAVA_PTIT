02b9d11aee4b815906fd50d6323551aaf4d0b9b2
phamduytb <duyxt2000@gmail.com>
2022-10-04 08:31:37 +0700

    push J02

---
+++ J02-Array/src/array/SoLienKe.java
@@ -0,0 +1,16 @@
+package array;
+
+import java.util.Scanner;
+
+public class SoLienKe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t;
+        t = sc.nextInt();
+        for(int i = 0 ; i < t; i++){
+            int a[] = new int[20];
+            int input,j=0;
+            input= sc.nextL
+        }
+    }
+}
